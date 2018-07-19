c08823ee9ac83d1549a1ae9a83f3ca9e6a1c608e
thinkreed <thinkreed2017@outlook.com>
2018-07-20 07:42:10 +0800

    step 21 card layout

--- steps/layout_step/lib/main.dart
+++ steps/layout_step/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-import 's20_stack.dart';
+import 's21_card_layout.dart';
 
 void main() {
   debugPaintSizeEnabled = true;
-  runApp(StackApp());
+  runApp(CardApp());
 }
