29040a5debed0c42ed2e389cd4dedc795096da99
thinkreed <thinkreed2017@outlook.com>
2018-07-29 13:09:21 +0800

    step 28 curve animation

--- steps/layout_step/lib/main.dart
+++ steps/layout_step/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-import 's27_animated_builder.dart';
+import 's28_curve_animation.dart';
 
 void main() {
   debugPaintSizeEnabled = true;
