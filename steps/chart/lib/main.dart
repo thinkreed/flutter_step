05d67efbb59737be634aa5b8e91ac40ccd6d5d3d
thinkreed <thinkreed2017@outlook.com>
2018-07-31 07:15:08 +0800

    step 2 bar chart

--- steps/chart/lib/main.dart
+++ steps/chart/lib/main.dart
@@ -1,36 +1,8 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 's2_bar_chart.dart';
 
 void main() {
   runApp(new MaterialApp(home: new ChartPage()));
-}
-
-class ChartPage extends StatefulWidget {
-  @override
-  ChartPageState createState() => new ChartPageState();
-}
-
-class ChartPageState extends State<ChartPage> {
-  final random = new Random();
-  int dataSet;
-  
-  void changeData() {
-    setState(() {
-      dataSet = random.nextInt(100);
-    });
-  }
-  
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Center(
-        child: new Text('Data set: $dataSet'),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        child: new Icon(Icons.refresh),
-        onPressed: changeData,
-      ),
-    );
-  }
 }
